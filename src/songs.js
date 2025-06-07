c9e171dd0f62be3cb7a998cc957710ca59b7eab3
maggyleet <mahaengineer7@gmail.com>
2025-06-07 22:18:25 +0530

    add songs - ms

--- src/songs.js
+++ src/songs.js
@@ -1,6 +1,17 @@
 import lifeOfRam from './assets/Life-of-Ram.mp3';
 import nenjukulle from './assets/Nenjukulle.mp3';
 import mayaNadhi from './assets/Maya-Nadhi.mp3';
+import heySinamika from './assets/Hey_Sinamika.mp3'
+import journey from './assets/Journey.mp3'
+import moongilThottam from './assets/Kadal_Moongil_Thottam.mp3'
+import laaliLaali from './assets/Laali_Laali.mp3'
+import marudhaani from './assets/Marudhaani.mp3'
+import kodiAruvi from './assets/kodiAruvi.mp3'
+import neethanae from './assets/Neethaanae_Mersal.mp3'
+import naan_un from './assets/Naan_un.mp3'
+import nilaneevaanam from './assets/Nila-Nee-Vaanam.mp3'
+import agastha from './assets/Agasatha.mp3'
+
 
 const songs = [
     {
@@ -17,6 +28,56 @@ const songs = [
         title: "Maya Nadhi",
         artist: "Pradeep Kumar, Swetha Mohan, Santhosh Narayanan",
         src: mayaNadhi,
+    },
+    {
+        title: "Hey Sinamika",
+        artist: "Pradeep Kumar, Govind Vasantha",
+        src: heySinamika,
+    },
+    {
+        title: "Journey",
+        artist: "Pradeep Kumar",
+        src: journey,
+    },
+    {
+        title: "Moongil Thottam",
+        artist: "Abhay Jodhpurkar, A R Rahman, Harini",
+        src: moongilThottam,
+    },
+    {
+        title: "Laali Laali",
+        artist: "Sathyaprakash, Pragathi Guruprasad, Ghibran",
+        src: laaliLaali,
+    },
+    {
+        title: "Marudhaani",
+        artist: "Madhushree, A R Rahman",
+        src: marudhaani,
+    },
+    {
+        title: "Naan Un",
+        artist: "Arjit Singh, Chinmayi Sripada, A R Rahman",
+        src: naan_un,
+    },
+    {
+        title: "Kodi Aruvi",
+        artist: "Pradeep Kumar, Nithyasree Venkatramanan, Sean Roldan",
+        src: kodiAruvi,
+    },
+    {
+        title: "Neethanae",
+        artist: "Shreya Ghosal, A R Rahman",
+        src: neethanae,
+    },
+    {
+        title: "Nila Nee Vaanam",
+        artist: "Vijay Yesudas, Chinmayi Sripada",
+        src: nilaneevaanam,
+    },
+    {
+        title: "Agasatha",
+        artist: "Pradeep Kumar, Kalyani Nair, Santhosh Narayanan",
+        src: agastha,
     },
 ];
 
